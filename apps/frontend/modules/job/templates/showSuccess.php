f318df19be9a419e121f5be84940b88dcc3bcd0c
OharaKohei <kohei@Ohara.local>
2014-10-03 17:46:42 +0900

    imagesが適用されないのを改善

--- apps/frontend/modules/job/templates/showSuccess.php
+++ apps/frontend/modules/job/templates/showSuccess.php
@@ -16,7 +16,7 @@
   <?php if ($job->getLogo()): ?>
     <div class="logo">
       <a href="<?php echo $job->getUrl() ?>">
-        <img src="/uploads/jobs/<?php echo $job->getLogo() ?>"
+        <img src="/jobeet/uploads/jobs/<?php echo $job->getLogo() ?>"
           alt="<?php echo $job->getCompany() ?> logo" />
       </a>
     </div>
