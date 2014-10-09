aa7b8c45cd948e447f20eef101b4ea05396ca4a7
OharaKohei <kohei@Ohara.local>
2014-10-10 00:05:53 +0900

    7thday 終了まで

--- lib/model/doctrine/JobeetCategory.class.php
+++ lib/model/doctrine/JobeetCategory.class.php
@@ -12,13 +12,30 @@
  */
 class JobeetCategory extends BaseJobeetCategory
 {
-    public function getActiveJobs($max)
+    public function getActiveJobs($max = 10)
+    {
+      $q = $this->getActiveJobsQuery()
+        ->limit($max);
+
+      return $q->execute();
+    }
+
+    // public function getSlug()
+    // {
+    //   return Jobeet::slugify($this->getName());
+    // }
+
+    public function countActiveJobs()
+    {
+      return $this->getActiveJobsQuery()->count();
+    }
+
+    public function getActiveJobsQuery()
     {
       $q = Doctrine_Query::create()
         ->from('JobeetJob j')
-        ->where('j.category_id = ?', $this->getId())
-        ->limit($max);
+        ->where('j.category_id = ?', $this->getId());
 
-      return Doctrine_Core::getTable('JobeetJob')->getActiveJobs($q);
+      return Doctrine_Core::getTable('JobeetJob')->addActiveJobsQuery($q);
     }
 }
